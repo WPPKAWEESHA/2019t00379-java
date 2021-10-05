dca0c0ef47db54d0bce1ca0ae0a4216308c421c3
Kaweesha <pawankaweesha123@gmail.com>
2021-10-06 01:30:23 +0800

    First commit

--- IA2211Lec2/src/com/company/Main.java
+++ IA2211Lec2/src/com/company/Main.java
@@ -2,7 +2,12 @@ package com.company;
 
 public class Main {
 
+     static String name = "Kaweesha";
+
+
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello World");
+        System.out.println("Name:" + name);
     }
 }
